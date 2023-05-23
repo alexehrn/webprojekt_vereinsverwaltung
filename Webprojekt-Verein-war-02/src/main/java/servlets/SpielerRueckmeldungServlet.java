3b6e771f2787753ecf92fe478c8b8175330f0ad7
fabia <fabia@LAPTOP-1VFHB77F>
2023-05-23 14:12:14 +0200

    Rückmeldung erweitert

--- Webprojekt-Verein-war-02/src/main/java/servlets/SpielerRueckmeldungServlet.java
+++ Webprojekt-Verein-war-02/src/main/java/servlets/SpielerRueckmeldungServlet.java
@@ -1,6 +1,5 @@
 package servlets;
 
-
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -18,66 +17,103 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 
-
-
 @WebServlet("/SpielerRueckmeldungServlet")
 public class SpielerRueckmeldungServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	@Resource(lookup="java:jboss/datasources/MySqlThidbDS")
+	@Resource(lookup = "java:jboss/datasources/MySqlThidbDS")
 	private DataSource ds;
-	
-    public SpielerRueckmeldungServlet() {
-    	super();
-    }
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		
-		
-		//Spieler aus Session holen
-		HttpSession session = request.getSession();
-		SpielerBean spieler = (SpielerBean) session.getAttribute("spieler");
-		
-		RueckmeldungsBean rueckmeldung = new RueckmeldungsBean();
-		
-		rueckmeldung.setRueckmeldung(request.getParameter("rueckmeldung"));
-		Long terminId = Long.valueOf(request.getParameter("id"));
-		
-		
-		
-		//falls noch keine Rückmeldung gegeben wurde --> rueckmeldung == Keine Rückmeldung
-		rueckmeldungAnlegen(rueckmeldung, spieler, terminId);
-		
-		session.setAttribute("rueckmeldung", rueckmeldung); //braucht man das?
-		
-		response.sendRedirect("SearchServletSpielerHome");
-		
+	public SpielerRueckmeldungServlet() {
+		super();
 	}
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
 
-	
-	private void rueckmeldungAnlegen(RueckmeldungsBean rueckmeldung,SpielerBean spieler, Long terminId)  throws ServletException {
-		
-		// DB-Zugriff
-		String[] generatedKeys = new String[] {"id"};	// Name der Spalte(n), die automatisch generiert wird(werden)
-		
+		// Spieler aus Session holen
+		HttpSession session = request.getSession();
+		SpielerBean spieler = (SpielerBean) session.getAttribute("spieler");
+
+		RueckmeldungsBean rueckmeldung = new RueckmeldungsBean();
+
+		rueckmeldung.setRueckmeldung(request.getParameter("rueckmeldung"));
+		Long terminId = Long.valueOf(request.getParameter("id"));
+
+		// falls noch keine Rückmeldung gegeben wurde --> rueckmeldung == Keine Rückmeldung
+		if (rueckmeldungVorhanden(spieler, terminId)) {
+			rueckmeldungAktualisieren(rueckmeldung, spieler, terminId);
+		} else {
+			rueckmeldungAnlegen(rueckmeldung, spieler, terminId);
+		}
+
+	//	session.setAttribute("rueckmeldung", rueckmeldung); 
+
+		response.sendRedirect("SearchServletSpielerHome");
+
+	}
+
+	private boolean rueckmeldungVorhanden(SpielerBean spieler, Long terminId) throws ServletException {
+
 		try (Connection con = ds.getConnection();
-			PreparedStatement pstmt = con.prepareStatement(
-					"INSERT INTO rueckmeldung (spieler_id,termin_id, meldung) VALUES (?, ?, ?)", 
-					generatedKeys)){
-
+				PreparedStatement pstmt = con
+						.prepareStatement("SELECT * FROM rueckmeldung WHERE spieler_id=? AND termin_id=?")) {
 
 			// Zugriff über Klasse java.sql.PreparedStatement
-				
+			pstmt.setLong(1, spieler.getId());
+			pstmt.setLong(2, terminId);
+
+			try (ResultSet rs = pstmt.executeQuery()) {
+				if (rs.next()) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		} catch (Exception ex) {
+			throw new ServletException(ex.getMessage());
+		}
+	}
+
+	private void rueckmeldungAktualisieren(RueckmeldungsBean rueckmeldung, SpielerBean spieler, Long terminId)
+			throws ServletException {
+
+		// DB-Zugriff
+		try (Connection con = ds.getConnection();
+				PreparedStatement pstmt = con
+						.prepareStatement("Update rueckmeldung SET meldung=? WHERE spieler_id=? AND termin_id=?")) {
+
+			// Zugriff über Klasse java.sql.PreparedStatement
+			pstmt.setString(1, rueckmeldung.getRueckmeldung());
+			pstmt.setLong(2, spieler.getId());
+			pstmt.setLong(3, terminId);
+
+			pstmt.executeUpdate();
+
+		} catch (Exception ex) {
+			throw new ServletException(ex.getMessage());
+		}
+	}
+
+	private void rueckmeldungAnlegen(RueckmeldungsBean rueckmeldung, SpielerBean spieler, Long terminId)
+			throws ServletException {
+
+		// DB-Zugriff
+		String[] generatedKeys = new String[] { "id" }; // Name der Spalte(n), die automatisch generiert wird(werden)
+
+		try (Connection con = ds.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(
+						"INSERT INTO rueckmeldung (spieler_id,termin_id, meldung) VALUES (?, ?, ?)", generatedKeys)) {
+
+			// Zugriff über Klasse java.sql.PreparedStatement
+
 			pstmt.setLong(1, spieler.getId());
 			pstmt.setLong(2, terminId);
 			pstmt.setString(3, rueckmeldung.getRueckmeldung());
-			
+
 			pstmt.executeUpdate();
-			
-			
+
 			// Generierte(n) Schlüssel auslesen (funktioniert nur mit PreparedStatement)
 			try (ResultSet rs = pstmt.getGeneratedKeys()) {
 				int i = 1;
@@ -89,7 +125,9 @@ public class SpielerRueckmeldungServlet extends HttpServlet {
 			throw new ServletException(ex.getMessage());
 		}
 	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
