88b67ef6c504ab1d6284a6de3c23554fc2914c6a
ale5314 <130575296+ale5314@users.noreply.github.com>
2023-06-07 16:34:51 +0200

    LogOutServlet ergänzt, damit Beans aus Session genommen werden

---
+++ Webprojekt-Verein-war-02/src/main/java/servlets/LogOutServlet.java
@@ -0,0 +1,54 @@
+package servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LogOutServlet
+ */
+@WebServlet("/LogOutServlet")
+public class LogOutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LogOutServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		/* der folgende Aufruf 	session.invalidate(); wurde über Chat-GPT herausgefunden */
+		/* Durch den Aufruf der invalidate()-Methode wird die aktuelle Session ungültig gemacht und alle darin gespeicherten Attribute, einschließlich der JavaBeans, werden entfernt. */
+
+		
+		session.invalidate();
+		
+		
+		
+		
+		RequestDispatcher disp = request.getRequestDispatcher("./index.jsp");
+		disp.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
